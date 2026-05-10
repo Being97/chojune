bea21948ee576e82eb42739f249e9a34749038fd
Being97 <bjh9628@naver.com>
2026-05-10 16:11:35 +0900

    404 error

--- chojune-main/next.config.ts
+++ chojune-main/next.config.ts
@@ -1,7 +1,9 @@
+// next.config.ts
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // 배포 최적화 설정
 };
 
 export default nextConfig;
